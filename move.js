4be9dc425cde2c3281829c1b5d656b8cb8ce71b4
Mohammad Tahvili <mohammad.tahvili@mail.utoronto.ca>
2020-07-20 22:03:45 -0400

    first commit

---
+++ move.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+
+$("#back").click(function(){
+    $("#background").slideToggle("slow");
+  });
+
+$("#link").click(function(){
+    $("#linkpart").slideToggle("slow");
+  });
+
+$("#linkhover").click(function(){
+    $("#linkhoverpart").slideToggle("slow");
+  });
+
+$("#header").click(function(){
+    $("#headercode").slideToggle("slow");
+  });
+
+$("#menu").click(function(){
+    $("#menucode").slideToggle("slow");
+  });
+
+$("#post").click(function(){
+    $("#postbody").slideToggle("slow");
+  });
+
+$("#soilder").click(function(){
+    $("#soildertbody").slideToggle("slow");
+  });
+
+$("#title").click(function(){
+    $("#titlebody").slideToggle("slow");
+  });
+
+$("#info").click(function(){
+    $("#info1").slideToggle("slow");
+  });
+
+$("#botttom").click(function(){
+    $("#botttompart").slideToggle("slow");
+  });
+
+$("#code").click(function(){
+    $("#getcode").slideToggle("slow");
+  });
+});
